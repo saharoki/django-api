fdaebe2442ce2c7ca5a4d4ad4872a199f2fa524e
saharoki <80281778+saharoki@users.noreply.github.com>
2021-12-24 08:35:02 +0200

    movie list

--- djangoapi/views.py
+++ djangoapi/views.py
@@ -2,9 +2,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Movie
 from .serializers import MovieSerializer
+from rest_framework.renderers import JSONRenderer
+
 
 class MovieListView(APIView):
-
+    renderer_classes = [JSONRenderer]
     def get(self, request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
