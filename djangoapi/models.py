19ad2f8413d42e0c864174df5d655cd0a1e3a50e
saharoki <80281778+saharoki@users.noreply.github.com>
2021-12-24 06:03:08 +0200

    movie list

--- djangoapi/models.py
+++ djangoapi/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Movie(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     release_date = models.DateField()
     overview = models.TextField()
     image = models.BinaryField()
     score = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         app_label = 'djangoapi'
