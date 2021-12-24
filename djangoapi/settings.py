17ababd896373a1f9402965ffb7289a8c663a9f5
saharoki <80281778+saharoki@users.noreply.github.com>
2021-12-24 05:00:32 +0200

    connect db

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'djangoapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movies_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin123321',
+        'HOST': 'movies-db.cluster-cppq7pdgfrly.eu-central-1.rds.amazonaws.com',
+        'PORT': 3306
     }
 }
 
